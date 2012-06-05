d64bb68494fda66a80046242a0900cd77a0d1051
Etienne Millon <me@emillon.org>
2012-06-05 17:34:32 +0200

    Use Pandoc writer options

--- wiki.hs
+++ wiki.hs
@@ -1,29 +1,42 @@
 import Data.Monoid
+import Hakyll.Core.Compiler
 import Hakyll.Core.Configuration
 import Hakyll.Core.Identifier.Pattern
+import Hakyll.Core.Resource
 import Hakyll.Core.Routes
 import Hakyll.Core.Rules
 import Hakyll.Core.Run
 import Hakyll.Web.Page
+import Hakyll.Web.Pandoc
+
+import Text.Pandoc.Shared
 
 main :: IO ()
 main = buildWiki
 
 buildWiki :: IO ()
 buildWiki = do
-  _ <- run conf rules
+  writerOpts <- getWriterOpts
+  _ <- run conf (rules writerOpts)
   return ()
+
+getWriterOpts :: IO WriterOptions
+getWriterOpts = return defaultWriterOptions
 
 conf :: HakyllConfiguration
 conf = defaultHakyllConfiguration
 
-rules :: RulesM ()
+rules :: WriterOptions -> RulesM ()
 rules = do
   compileMarkdown
 
-compileMarkdown :: Rules
-compileMarkdown = do
-  _ <- match (parseGlob "src/*") $ do
+compileMarkdown :: WriterOptions -> Rules
+compileMarkdown wo = do
+  _ <- match (parseGlob "wiki/*") $ do
     route $ setExtension ".html"
-    compile pageCompiler
+    compile (pdcCompiler wo)
   return ()
+
+pdcCompiler :: WriterOptions -> Compiler Resource (Page String)
+pdcCompiler writerOpts =
+  pageCompilerWith defaultHakyllParserState writerOpts
